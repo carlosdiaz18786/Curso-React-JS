79ff28bebf968d953d8547350b914591f3d70a5b
Carlos Diaz <carlos18786@hotmail.com>
2019-04-10 15:52:11 -0500

    Seccion 6 Finalizada

--- weather-app/src/components/WeatherLocation/WeatherData/index.js
+++ weather-app/src/components/WeatherLocation/WeatherData/index.js
@@ -2,20 +2,24 @@ import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from '../WeatherData/WeatherTemperature';
 import './styles.css';
-import {
-    CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN,
-    SNOW,
-    WINDY
-} from '../../../constants/weathers';
+import PropTypes from 'prop-types';
+
 //clasName es para darle clase al elemento html
-const WeatherData = () => (
-    <div className="weatherDataCont"> 
-        <WeatherTemperature temperature={20} weatherState={CLOUDY}></WeatherTemperature>
-        <WeatherExtraInfo humidity={80} wind={"10 m/s"}></WeatherExtraInfo>
+const WeatherData = ({ data: {temperature, weatherState, humidity, wind}}) => (
+   <div className="weatherDataCont"> 
+        <WeatherTemperature temperature={temperature} weatherState={weatherState}></WeatherTemperature>
+        <WeatherExtraInfo humidity={humidity} wind={wind}></WeatherExtraInfo>
     </div>
 );
+
+//Queremos que la data venga como un objeto con parametros establecidos
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    })
+};
 
 export default WeatherData;
--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY
+} from '../../constants/weathers';
 
-const WeatherLocation = () => ( 
-    <div className="weatherLocationCont">
-        <Location city={"Bogotá"}></Location>
-        <WeatherData></WeatherData>
-    </div>
-);
+const data = {
+    temperature:5,
+    weatherState:SUN,
+    humidity: 10,
+    wind: '10 m/s',
+};
+
+const data2 = {
+    temperature:19,
+    weatherState:WINDY,
+    humidity: 8,
+    wind: '19 m/s',
+};
+
+//data es un parametro que pasamos a WeatherData
+class WeatherLocation  extends Component {
+
+    constructor() {
+        super(); //llamada al componente base siempre hay que ponerlo
+        this.state = { 
+            city: 'Buenos Aires',
+            data:data,
+        };
+    }
+    
+    handleUpdateClick = () =>{
+        this.setState({ //Lo utilizamos para setear el state si usamos state= no funciona
+            city: 'Soachington D.C',
+            data:data2,
+        });
+    }
+
+    render() {
+        const {city, data} = this.state;
+        return (
+            <div className="weatherLocationCont">
+                <Location city={city}></Location>
+                <WeatherData data={data}></WeatherData>
+                <button onClick={this.handleUpdateClick} >Actualizar</button>
+             </div>
+        );
+    }
+}
 
 export default WeatherLocation;
