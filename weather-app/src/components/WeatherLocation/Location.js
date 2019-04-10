0c7996f281199984fbab9cad8fff27848de78e03
Carlos Diaz <parcerocarl@gmail.com>
2019-04-09 20:56:30 -0500

    Seccion 5 Finalizada

--- weather-app/src/components/WeatherLocation/Location.js
+++ weather-app/src/components/WeatherLocation/Location.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 const Location = ({city}) => (
    //debugger; //punto de interrupcion
@@ -8,7 +9,7 @@ const Location = ({city}) => (
     const { city } = props;*/
 
     
-    <div>
+    <div className="locationCont">
         <h1>{city}</h1>
     </div>
 );
--- weather-app/src/components/WeatherLocation/WeatherData/index.js
+++ weather-app/src/components/WeatherLocation/WeatherData/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from '../WeatherData/WeatherTemperature';
+import './styles.css';
 import {
     CLOUD,
     CLOUDY,
@@ -9,9 +10,9 @@ import {
     SNOW,
     WINDY
 } from '../../../constants/weathers';
-
+//clasName es para darle clase al elemento html
 const WeatherData = () => (
-    <div>
+    <div className="weatherDataCont"> 
         <WeatherTemperature temperature={20} weatherState={CLOUDY}></WeatherTemperature>
         <WeatherExtraInfo humidity={80} wind={"10 m/s"}></WeatherExtraInfo>
     </div>
